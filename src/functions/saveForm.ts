46d06a0b201584285ae5fa8897add3cfcb6810ee
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-12-09 05:21:01 +0530

    Update saveForm.ts

--- src/functions/saveForm.ts
+++ src/functions/saveForm.ts
@@ -13,23 +13,20 @@ export const saveForm = async (
   if (!userId) throw new ApiError("Refresh required", 401);
   const data = req.body;
   const form = validateFrom(data.questions);
-  if (form.isValid) {
-    const savedForm = await Form.findOneAndUpdate({ owner: userId }, data, {
-      new: true,
-      upsert: true,
-    }).select("-updatedAt -createdAt");
-
-    res.cookie("guest_form", String(savedForm._id), {
-      httpOnly: true,
-      sameSite: "none",
-      secure: true,
-      path: "/",
-      expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
-    });
-    deleteCacheByPattern("submitted");
-    memoryCache.set(userId + "form", savedForm);
-    memoryCache.set(String(savedForm._id), savedForm);
-    return res.send(String(savedForm._id));
-  }
-  return res.status(400).json(form);
+  if (!form.isValid) throw new ApiError("Invalid form", 400, form.errors);
+  const savedForm = await Form.findOneAndUpdate({ owner: userId }, data, {
+    new: true,
+    upsert: true,
+  }).select("-updatedAt -createdAt");
+  memoryCache.set(userId + "form", savedForm);
+  memoryCache.set(String(savedForm._id), savedForm);
+  res.cookie("guest_form", String(savedForm._id), {
+    httpOnly: true,
+    sameSite: "none",
+    secure: true,
+    path: "/",
+    expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+  });
+  deleteCacheByPattern("submitted");
+  return res.send(String(savedForm._id));
 };
